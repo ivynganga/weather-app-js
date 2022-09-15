32fbcbe3228888bd01a6446f0c4a8e102c4bfc74
Ivy <28644397+ivynganga@users.noreply.github.com>
2022-09-15 21:28:37 +0300

    initial commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+let id = (id) => document.getElementById(id);
+
+let form = id('form');
+let city = id('city');
+let country = id('country');
+let submit = id('submit');
+
+const apikey = '';
+
+<<<<<<< HEAD
+=======
+console.log('city = ' + city.value);
+>>>>>>> 2c6cce2df5e1e75e57e48fdb3698cda1bdc72fcf
+if (form !== undefined && form !== null && submit !== undefined && submit !== null){
+	form.addEventListener('submit', (e) => {e.preventDefault();
+		if (city.value === '' || country.value === '' )  {
+			alert('city and country are mandatory');
+		}
+		else {
+			alert('success');
+		}
+	});
+}
+else {
+	alert('something bad happened. please try again.');
+}
+
+
+
+
+function weather(latitude, longitude) {
+	const url = 'https://api.openweathermap.org/data/2.5/weather?lat=' + latitude + '&lon=' + longitude + '&appid=' + apikey + '&units=metric';
+
+
+}
+
+function forecast(latitude, longitude) {
+	const url = 'api.openweathermap.org/data/2.5/forecast?lat=' + latitude + '&lon=' + longitude + '&appid=' + apikey + '&units=metric';
+	
+}
